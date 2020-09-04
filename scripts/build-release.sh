01874907e1c6fac5b5e5c38517256d2f3bb0ca8c
Jose Fuentes <jsfuentescastillo@gmail.com>
2020-09-04 14:39:53 +0200

    Make released binary be statically linked

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -29,7 +29,7 @@ do
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -v -o $output_name
+    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -v -o $output_name
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
